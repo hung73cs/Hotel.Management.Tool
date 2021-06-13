d2ab672a76bb0b38bc43dcb6404ad62b563ae443
hung73cs <hung73.cs@gmail.com>
2021-06-13 13:15:35 +0700

    using page404 500 in router

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -47,8 +47,8 @@ const Toasts = React.lazy(() => import('./views/components/notifications/toasts/
 
 const Login = React.lazy(() => import('./views/pages/login/Login'))
 // const Register = React.lazy(() => import('./views/examples/pages/register/Register'))
-// const Page404 = React.lazy(() => import('./views/examples/pages/page404/Page404'))
-// const Page500 = React.lazy(() => import('./views/examples/pages/page500/Page500'))
+const Page404 = React.lazy(() => import('./views/examples/pages/page404/Page404'))
+const Page500 = React.lazy(() => import('./views/examples/pages/page500/Page500'))
 
 const Widgets = React.lazy(() => import('./views/components/widgets/Widgets'))
 
@@ -102,10 +102,10 @@ const routes = [
   { path: '/notifications/badges', name: 'Badges', component: Badges },
   { path: '/notifications/modals', name: 'Modals', component: Modals },
   { path: '/notifications/toasts', name: 'Toasts', component: Toasts },
-  // { path: '/login', name: 'Login', component: Login },
+  { path: '/login', name: 'Login', component: Login },
   // { path: '/register', name: 'Register', component: Register },
-  // { path: '/404', name: '404', component: Page404 },
-  // { path: '/500', name: '500', component: Page500 },
+  { path: '/404', name: '404', component: Page404 },
+  { path: '/500', name: '500', component: Page500 },
   { path: '/widgets', name: 'Widgets', component: Widgets },
 
   //Tự thêm
