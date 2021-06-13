5dee8f44520aaf188689dfa8476a3e707f4a9186
hung73cs <46376034+hung73cs@users.noreply.github.com>
2021-06-13 13:12:07 +0700

    update css for login page

--- Frontend/src/views/pages/login/Login.js
+++ Frontend/src/views/pages/login/Login.js
@@ -54,66 +54,43 @@ const Login = () => {
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
         <CRow className="justify-content-center">
-          <CCol md="8">
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm onSubmit={handleSubmit}>
-                    <h1>Login</h1>
-                    <p className="text-medium-emphasis">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon name="cil-user" />
-                      </CInputGroupText>
-                      <CFormControl
-                        placeholder="Username"
-                        autoComplete="username"
-                        value={username}
-                        onInput={(e) => handleChange(e)}
-                      />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon name="cil-lock-locked" />
-                      </CInputGroupText>
-                      <CFormControl
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                      />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs="6">
-                        <CButton color="primary" className="px-4">
-                          Login
-                        </CButton>
-                      </CCol>
-                      <CCol xs="6" className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
+          <CCol md="5">
+            <CCard className="p-4">
+              <CCardBody>
+                <CForm onSubmit={handleSubmit}>
+                  <h1>Login</h1>
+                  <p className="text-medium-emphasis">Sign In to your account</p>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon name="cil-user" />
+                    </CInputGroupText>
+                    <CFormControl
+                      placeholder="Username"
+                      autoComplete="username"
+                      value={username}
+                      onInput={(e) => handleChange(e)}
+                    />
+                  </CInputGroup>
+                  <CInputGroup className="mb-4">
+                    <CInputGroupText>
+                      <CIcon name="cil-lock-locked" />
+                    </CInputGroupText>
+                    <CFormControl
+                      type="password"
+                      placeholder="Password"
+                      autoComplete="current-password"
+                    />
+                  </CInputGroup>
+                  <CRow>
+                    <CCol xs="6">
+                      <CButton color="primary" className="px-4">
+                        Login
                       </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
+                    </CCol>
+                  </CRow>
+                </CForm>
+              </CCardBody>
+            </CCard>
           </CCol>
         </CRow>
       </CContainer>
