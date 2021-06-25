6cc35689baa4a10c17b26968a27807be8d9d158d
hung73cs <hung73.cs@gmail.com>
2021-06-25 19:34:44 +0700

    sửa tạo room

---
+++ Backend/Hotel-Management-Tool/ApplicationLogic/BillService.cs
@@ -0,0 +1,42 @@
+﻿using Hotel.Management.Tool.Core.Entities;
+using Hotel.Management.Tool.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Management.Tool.ApplicationLogic
+{
+    public class BillService : IBillService
+    {
+        public Task<Bill> CreateAsync(Bill accountRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteBill(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bill> GetBill(Guid bookingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Bill>> GetBills()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HardDeleteBill(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bill> UpdateBill(Bill bookingToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Backend/Hotel-Management-Tool/Presentation/Controllers/BillController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Management.Tool.Presentation.Controllers
+{
+    [Authorize]
+    [Route("bill")]
+    public class BillController : ControllerBase
+    {
+    }
+}
