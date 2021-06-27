05168168f79e85ecf3de2fe4fd634a54a99052cd
LaiVoJM <19521733@gm.uit.edu.vn>
2021-06-27 20:19:00 +0700

    Update SurchargeRateController.cs

--- Backend/Hotel-Management-Tool/Presentation/Controllers/SurchargeRateController.cs
+++ Backend/Hotel-Management-Tool/Presentation/Controllers/SurchargeRateController.cs
@@ -7,6 +7,7 @@ using Hotel.Management.Tool.Presentation.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Hotel.Management.Tool.Presentation.Controllers
 {
@@ -37,17 +38,6 @@ namespace Hotel.Management.Tool.Presentation.Controllers
                 _surchargeRateMapper.MapSurchargeRateToSurchargeRateModel(surchargeRate));
         }
 
-        [HttpGet]
-        public async Task<ActionResult<SurchargeRateModel>> GetSurchargeRates()
-        {
-            var surchargeRates = await _surchargeRateService.GetSurchargeRatesAsync();
-            if (surchargeRates == null)
-            {
-                throw new ExtendException(ErrorCode.NotFound, CommonConstants.ErrorMessage.ItemNotFound);
-            }
-            return Ok(_surchargeRateMapper.MapSurchargeRateToSurchargeRateModel(surchargeRates));
-        }
-
         [HttpPost]
         public async Task<ActionResult> CreateSurchargeRate([FromBody] SurchargeRateModel surchargeRateModel)
         {
@@ -57,6 +47,27 @@ namespace Hotel.Management.Tool.Presentation.Controllers
                 throw new ExtendException(ErrorCode.Conflict, CommonConstants.ErrorMessage.WrongMapping);
             }
             var result = await _surchargeRateService.CreateSurchargeRateAsync(mapper);
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        [Route("id/{surchargeRateId}")]
+        public async Task<ActionResult> UpdateSurchargeRate(int guestLevel, [FromBody] SurchargeRateModel surchargeRateModel)
+        {
+            var currentSurchargeRate = await _surchargeRateService.GetSurchargeRateAsync(guestLevel);
+
+            if (currentSurchargeRate == null)
+            {
+                throw new ExtendException(ErrorCode.NotFound, CommonConstants.ErrorMessage.ItemNotFound);
+            }
+            var mapper = _surchargeRateMapper.MapSurchargeRateModelToSurchargeRate(surchargeRateModel, currentSurchargeRate);
+
+            if (mapper == null)
+            {
+                throw new ExtendException(ErrorCode.Conflict, CommonConstants.ErrorMessage.WrongMapping);
+            }
+            await _surchargeRateService.UpdateSurchargeRateAsync(mapper);
 
             return NoContent();
         }
@@ -80,6 +91,17 @@ namespace Hotel.Management.Tool.Presentation.Controllers
                 {
                     Result = result
                 });
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SurchargeRateModel>>> GetSurchargeRates()
+        {
+            var surchargeRates = await _surchargeRateService.GetSurchargeRatesAsync();
+            if (surchargeRates == null)
+            {
+                throw new ExtendException(ErrorCode.NotFound, CommonConstants.ErrorMessage.ItemNotFound);
+            }
+            return Ok(_surchargeRateMapper.MapSurchargeRateToSurchargeRateModel(surchargeRates));
         }
     }
 }
